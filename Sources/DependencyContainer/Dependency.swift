004429acd125b5d074070cf14a3e0714b7dd3c55
ChandanChilamkurthy <radhachandan.chilamkurthy@prowesssoft.com>
2021-02-01 13:34:33 +0530

    Commit w.r.t changes

--- Sources/DependencyContainer/Dependency.swift
+++ Sources/DependencyContainer/Dependency.swift
@@ -1,12 +1,12 @@
 public struct Dependency {
     
-    typealias ResolveBlock<T> = () -> T
+    public typealias ResolveBlock<T> = () -> T
     
     private(set) var value: Any!
     private let resolveBlock: ResolveBlock<Any>
     let name: String
     
-    init<T>(_ block: @escaping ResolveBlock<T>) {
+    public init<T>(_ block: @escaping ResolveBlock<T>) {
         resolveBlock = block
         name = String(describing: T.self)
     }
